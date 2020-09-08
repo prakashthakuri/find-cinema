83bfe81238d326348f4187910b0515aa4dc77f4e
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-07 23:13:42 -0400

    Displaying the fetched information on the browser

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -39,16 +39,36 @@ export default function SearchMovies() {
     }
 
     return(
-        <div>
+        <>
             <form className="form" onSubmit= {searchMovies}>
                 <label className="label" htmlFor="query">Movie Name:  <br/>
                     <input className="input"  type="text" name="query" placeholder="Seach Movies"  value= {query}  onChange={(e) =>setQuery(e.target.value)}/>
                     
                     <br/>
+                    <br/>
 
                     <button className="button" type="submit">Search</button>
                 </label>
             </form>
-        </div>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => 
+                    (
+                        <div className="card" key={movie.id}>
+                            <img className="card--image" src= {`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + 'poster'} />
+                        
+                            <div className="card--content">
+                                <h3 className="card--title">{movie.title}</h3>
+                                <p><small>Release Date: { movie.release_date}</small></p>
+                                <p><small>Rating: { movie.vote_average}</small></p>
+                                 <p className="card--description">{movie.overview}</p>
+                            </div>
+                        
+                        </div>
+
+
+                    )
+                        )}
+            </div>
+        </>
     )
 }
