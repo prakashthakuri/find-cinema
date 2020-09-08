0c5a1f2fd2cabe0c2109224a246c2b8953cd4a5f
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-07 23:45:38 -0400

    breaking the component and finishing the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchMovies from './components/SearchMovies';
 
 function App() {
   return (
     <div>
-      <h1>
-      Find Cinema</h1>
+      <h1>Find Cinema</h1>
 
      <SearchMovies />
 
--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../css/SearchMovies.css'
+import MovieCard from './movieCard'
 
 
 export default function SearchMovies() {
@@ -42,7 +43,8 @@ export default function SearchMovies() {
 
     return(
         <>
-            <form className="form" onSubmit= {searchMovies}>
+
+<form className="form" onSubmit= {searchMovies}>
                 <label className="label" htmlFor="query">Movie Name:  <br/>
                     <input className="input"  type="text" name="query" placeholder="Seach Movies"  value= {query}  onChange={(e) =>setQuery(e.target.value)}/>
                     
@@ -55,17 +57,7 @@ export default function SearchMovies() {
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => 
                     (
-                        <div className="card" key={movie.id}>
-                            <img className="card--image" src= {`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + 'poster'} />
-                        
-                            <div className="card--content">
-                                <h3 className="card--title">{movie.title}</h3>
-                                <p><small>Release Date: { movie.release_date}</small></p>
-                                <p><small>Rating: { movie.vote_average}</small></p>
-                                 <p className="card--description">{movie.overview}</p>
-                            </div>
-                        
-                        </div>
+                        <MovieCard movie= {movie} key= {movie.id}/>
 
 
                     )
